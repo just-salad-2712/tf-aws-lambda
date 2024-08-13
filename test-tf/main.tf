7abdbcf447072c3c01b6ad7a1a93fcfafdd9b7a2
hieutm2712 <hieutm2712@personal.com>
2024-08-13 15:21:58 +0700

    fix

--- test-tf/main.tf
+++ test-tf/main.tf
@@ -1,4 +1,4 @@
-# Test Lambda Fucntion helloworld voi URL
+# Test Lambda Fucntion helloworld with URL
 
 provider "aws" {
   region = "ap-southeast-1"
@@ -11,20 +11,19 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-# data "aws_organizations_organization" "this" {}
-
 module "lambda_function" {
   source = "../"
 
   function_name = "Lambdatest"
   description   = "Testing lambda fuction"
+  create_role = false
   lambda_role   = "arn:aws:iam::637423165302:role/Lambda_Admin"
   handler       = "hello_world.lambda_handler"
   source_path   = "hello_world.py"
   runtime       = "python3.12"
 
   tags = {
-    Name = "my-lambda1"
+    Name = "test-lambda"
   }
 
   create_lambda_function_url = true
